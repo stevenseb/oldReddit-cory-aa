1b230a764827e8ba7d75d357729d41c21c3c63f0
Cory Kane <ckane30691@gmail.com>
2024-08-24 21:12:34 -0700

    Add routes/api folder and setup initial app

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+
 const port = process.env.PORT || 5000;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
 	.connect(db)
 	.then(() => console.log('Connected to MongoDB succesfully'))
 	.catch((err) => console.log(err));
+
+app.use('/api/users', users);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

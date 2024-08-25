c88d04dc92e93ab2c265e53651c0c966ef5f146e
Cory Kane <ckane30691@gmail.com>
2024-08-25 13:16:26 -0700

    Fix passport config

--- config/passport.js
+++ config/passport.js
@@ -2,17 +2,25 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
-const keys = require('../config/keys');
+const keys = require('./private/secret');
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = keys.secretOrKey;
+options.secretOrKey = keys;
 
 module.exports = (passport) => {
 	passport.use(
 		new JwtStrategy(options, (payload, done) => {
-			// This payload includes the items we specified earlier
-			console.log(payload);
+			User.findById(payload.id)
+				.then((user) => {
+					if (user) {
+						// return the user to the frontend
+						return done(null, user);
+					}
+					// return false since there is no user
+					return done(null, false);
+				})
+				.catch((err) => console.log(err));
 		})
 	);
 };

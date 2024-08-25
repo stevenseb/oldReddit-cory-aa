ac335ee87ca0dd40652a45d7ea4eec56130aadff
Cory Kane <ckane30691@gmail.com>
2024-08-25 12:40:16 -0700

    Write signup validator

---
+++ validation/signup.js
@@ -0,0 +1,33 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateRegisterInput(data) {
+	let errors = {};
+
+	data.username = !isEmpty(data.username) ? data.username : '';
+	data.email = !isEmpty(data.email) ? data.email : '';
+	data.password = !isEmpty(data.password) ? data.password : '';
+	data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+
+	if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
+		errors.username = 'Username must be between 2 and 30 characters';
+	}
+
+	if (Validator.isEmpty(data.username)) {
+		debugger;
+		errors.username = 'Username field is required';
+	}
+
+	if (Validator.isEmpty(data.password)) {
+		errors.password = 'Password field is required';
+	}
+
+	if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+		errors.password = 'Password must be at least 6 characters';
+	}
+
+	return {
+		errors,
+		isValid: isEmpty(errors),
+	};
+};

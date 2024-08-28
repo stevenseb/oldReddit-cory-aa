16cb9ebc53e553af81a1410c0410a33a9022db53
Cory Kane <ckane30691@gmail.com>
2024-08-27 20:12:23 -0700

    Clean up dead code

--- frontend/src/util/sessionApiUtil.js
+++ frontend/src/util/sessionApiUtil.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-// import jwt_decode from 'jwt-decode';
-// const { setCurrentUser } = require('../slices/sessionSlice');
-// const { getErrors } = require('../slices/sessionErrorsSlice');
 
 export const setAuthToken = (token) => {
 	if (token) {
@@ -12,49 +9,3 @@ export const setAuthToken = (token) => {
 		delete axios.defaults.headers.common['Authorization'];
 	}
 };
-
-// Register User
-// export const signUpUser = (userData, history) => async (dispatch) => {
-// 	try {
-// 		let res = await axios.post('/api/users/signup', userData);
-
-// 		const { token } = res.data;
-// 		// Set token to ls
-// 		localStorage.setItem('jwtToken', token);
-// 		// Set token to Auth header
-// 		setAuthToken(token);
-// 		// Decode token to get user data
-// 		const decoded = jwt_decode(token);
-// 		// Set current user
-// 		dispatch(setCurrentUser(decoded));
-// 	} catch (err) {
-// 		dispatch(getErrors(err));
-// 	}
-// };
-
-// Login - Get User Token
-// export const loginUser = (userData) => (dispatch) => {
-// 	try {
-// 		let res = axios.post('/api/users/login', userData);
-// 		const { token } = res.data;
-// 		// Set token to ls
-// 		localStorage.setItem('jwtToken', token);
-// 		// Set token to Auth header
-// 		setAuthToken(token);
-// 		// Decode token to get user data
-// 		const decoded = jwt_decode(token);
-// 		// Set current user
-// 		dispatch(setCurrentUser(decoded));
-// 	} catch (err) {
-// 		dispatch(getErrors(err));
-// 	}
-// };
-
-// export const logoutUser = () => (dispatch) => {
-// 	// Remove token from localStorage
-// 	localStorage.removeItem('jwtToken');
-// 	// Remove auth header for future requests
-// 	setAuthToken(false);
-// 	// Set current user to {} which will set isAuthenticated to false
-// 	dispatch(setCurrentUser({}));
-// };

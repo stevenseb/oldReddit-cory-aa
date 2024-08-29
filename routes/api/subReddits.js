fa1936d78262185c7f8124d4b05572598736eed3
Cory Kane <ckane30691@gmail.com>
2024-08-29 12:04:23 -0700

    Write subreddit show route

--- routes/api/subReddits.js
+++ routes/api/subReddits.js
@@ -15,3 +15,14 @@ router.get('/', async (req, res) => {
 		res.status(404).json({ noSubRedditsFound: 'No subReddits found' });
 	}
 });
+
+router.get('/:id', async (req, res) => {
+	try {
+		let subReddit = await SubReddit.findById(req.params.id);
+		res.json(subReddit);
+	} catch (err) {
+		res
+			.status(404)
+			.json({ noSubRedditFound: 'No subReddit found with that ID' });
+	}
+});

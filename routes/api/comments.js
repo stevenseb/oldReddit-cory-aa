45e74686e008918f6fc3ba672f236c6e7cf3b793
Cory Kane <ckane30691@gmail.com>
2024-08-30 15:56:23 -0700

    Write comment index and delete controllers

---
+++ routes/api/comments.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const Comment = require('../../models/Comment');
+const passport = require('passport');
+const validateCommentInput = require('../../validation/comment');
+
+const router = express.Router();
+
+module.exports = router;
+
+router.get('/', async (req, res) => {
+	try {
+		let comments = await Comment.find({ postId: req.body.postId });
+		res.json(comments);
+	} catch (err) {
+		res.status(404).json({ noCommentsFound: 'No comments yet' });
+	}
+});
+
+router.post(
+	'/',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		const { errors, isValid } = validateCommentInput(req.body);
+
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
+		try {
+			const comment = new Comment({
+				userId: req.user.id,
+				postId: req.body.postId,
+				body: req.body.body,
+				parentCommentId: req.body.parentCommentId,
+			});
+
+			await comment.save();
+			res.json(comment);
+		} catch (errors) {
+			res.status(400).json(errors);
+		}
+	}
+);
+
+router.delete(
+	'/:id',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		try {
+			let comment = await Comment.findById(req.params.id);
+			if (!comment)
+				return res
+					.status(404)
+					.json({ noCommentFound: "That comment doesn't exist" });
+			if (req.user.id == comment.userId) {
+				await comment.deleteOne();
+				return res.json(comment);
+			}
+			res.status(403).json({
+				notAuthorized: 'You did not make this comment',
+			});
+		} catch (errors) {
+			res.status(400).json(errors);
+		}
+	}
+);

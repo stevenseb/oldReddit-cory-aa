0c91e130ee07a28e84917e92745d1ccabbb01874
Cory Kane <ckane30691@gmail.com>
2024-09-07 16:25:54 -0700

    Add some more sidebar styling

--- frontend/src/components/sideBar.js
+++ frontend/src/components/sideBar.js
@@ -6,8 +6,20 @@ export const SideBar = () => {
 	return (
 		<div className="side-bar-container">
 			<Link className="side-bar-link" to={'/posts/new'}>
+				Submit a new link
+			</Link>
+			<Link className="side-bar-link" to={'/posts/new'}>
 				Submit a new text post
 			</Link>
+			<div className="premium-container">
+				<h1 className="prem-header">
+					OldReddit <span>Premium</span>
+				</h1>
+				<p>
+					Get an add free experience with special benefits, and directly support
+					OldReddit
+				</p>
+			</div>
 			<Link className="side-bar-link" to={'/subReddits/new'}>
 				Create your own subreddit
 			</Link>

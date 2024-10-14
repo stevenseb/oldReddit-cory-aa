c45b4786a513df89e5b0e2d2f5da7764eec15852
Cory Kane <ckane30691@gmail.com>
2024-10-13 23:15:11 -0700

    reconnect mongodb for testing

--- config/keysDev.js
+++ config/keysDev.js
@@ -1,5 +1,6 @@
 module.exports = {
 	mongoURI:
-		'mongodb+srv://root:714bCJK!@cluster0.7vonu.mongodb.net/Cluster0?retryWrites=true&w=majority',
+		'mongodb+srv://root:FOVTCU4OGkTwh6nh@cluster0.7vonu.mongodb.net/Cluster0?retryWrites=true&w=majority',
 	secretOrKey: 'secret',
 };
+
